b1301a0343142afcf93344f796441f9b11b9341b
Vipranan <vipranancr7@gmail.com>
2025-02-02 17:19:48 +0700

    new python code added

--- two.py
+++ two.py
@@ -1,6 +1,8 @@
 x = "vipranan"
-y = 2004
+y = str(2004)
 z = 20.9
+
+print(x[::-1])
 
 print(type(x))
 print(type(y))
@@ -8,3 +10,11 @@ print(type(z))
 
 v = True
 print(type(v))
+a = "HELLO IM LEARNING PYTHON"
+b = "hello im learning python"
+print(b.upper())
+print(a.lower())
+c = "  okay " 
+print(c)
+print(c.strip())
+print(b.replace("hello","hi"))
